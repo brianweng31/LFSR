e98f4140e07f5dd2c9232f294628e876162d755f
brianweng31 <brian.weng31@gmail.com>
2022-07-25 16:28:23 +0800

    7_25_16:28

--- eval.py
+++ eval.py
@@ -54,4 +54,9 @@ with torch.no_grad():
         else:
             light_field = np.concatenate((light_field, sr_refocused), axis=0)
 
+light_field = np.moveaxis(light_field, [2, 3, 4], [4, 2, 3])
 print(light_field.shape)
+
+for i in range(light_field.shape[0]):
+    cv2.imshow(i,light_field[i,5])
+    cv2.waitKey(0)
