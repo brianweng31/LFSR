257d10b6de383e794aae586003213f7cdb3b3986
brianweng31 <brian.weng31@gmail.com>
2022-08-02 17:57:52 +0800

    8_2_17:57

--- config.py
+++ config.py
@@ -25,10 +25,10 @@ refocused_img_metrics_name = ["PSNR", "SSIM", "GMSD", "LPIPS"]
 assert len(refocused_img_metrics) == len(refocused_img_metrics_name)
 
 ## training
-dataset_name = "HCI" # HCI, RandomTraining
-training_light_field_downsample_rate = [4,2,1]
+dataset_name = "RandomTraining" # HCI, RandomTraining
+training_light_field_downsample_rate = [2,1]
 #training_light_field_downsample_rate = [1]
-training_light_field_epoch = [40000,20000,20000]
+training_light_field_epoch = [20000,20000]
 #training_light_field_epoch = [30000]
 batch_size = 16
 assert len(training_light_field_downsample_rate)==len(training_light_field_epoch)
