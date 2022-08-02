50944d8d216506940f698a3b3125147ed3305f51
brianweng31 <brian.weng31@gmail.com>
2022-08-02 20:10:46 +0800

    8_2_20:10

--- main.py
+++ main.py
@@ -124,14 +124,15 @@ if __name__=="__main__":
 
                                     print(log_str)
                                     
-                                    test_loss = losses[0]
-                                    early_stopping(test_loss)
-                                    print(f"Last_loss:{early_stopping.last_loss}, Counter:{early_stopping.counter}")
-                                    if early_stopping.early_stop:
-                                        print("Early stopped at epoch: ", epoch)
-                                        methods[method_idx].record.tb_writer.close()
-                                        early_stopped[method_idx] = True
-                                        break
+                                    if epoch >= 500:
+                                        test_loss = losses[0]
+                                        early_stopping(test_loss)
+                                        print(f"Last_loss:{early_stopping.last_loss}, Counter:{early_stopping.counter}")
+                                        if early_stopping.early_stop:
+                                            print("Early stopped at epoch: ", epoch)
+                                            methods[method_idx].record.tb_writer.close()
+                                            early_stopped[method_idx] = True
+                                            break
                                     
 
 
