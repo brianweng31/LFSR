d8c9906beb64c16ce5b605b1841ff16e954d178d
brianweng31 <brian.weng31@gmail.com>
2022-08-02 15:47:24 +0800

    8_2_15:47

--- train.py
+++ train.py
@@ -26,6 +26,9 @@ def training(dataloader,device,methods,optimizers,optimized_losses,estimate_clea
                 sr = methods[method_idx].enhance_LR_lightfield(lr)
                 sr_refocused = refocus_pixel_focal_stack_batch(sr, dataloader.dataset.disparity_range, s, t)
                 sr_refocused = remove_img_margin(sr_refocused)
+                
+                print(f'sr_refocused.shape = {sr_refocused.shape}')
+                print(f'hr_refocused.shape = {hr_refocused.shape}')
 
                 losses = []
                 for optimized_losses_idx in range(len(optimized_losses)):
