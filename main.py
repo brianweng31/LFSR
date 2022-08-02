8afd1bc45b0c1bc47c6f70bea9b8e3862652d718
brianweng31 <brian.weng31@gmail.com>
2022-08-02 16:41:55 +0800

    8_2_16:41

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from model import BaselineMethod, FilterBankMethod, LinearFilter
 from train import training
 from test import testing
 
+
 torch.set_default_dtype(torch.float32)
 gc.collect()
 with torch.no_grad():
@@ -73,75 +74,75 @@ if __name__=="__main__":
             start = time.time()
             epochs = training_light_field_epoch[downsample_rate_idx]
             downsample_rate = training_light_field_downsample_rate[downsample_rate_idx]
-            #try:
-            #train_dataloader, test_dataloader = get_dataloaders('SRFilterTraining', batch_size=batch_size, downsample_rate=downsample_rate)
-            train_dataloader, test_dataloader = get_dataloaders(dataset_name, batch_size=batch_size, downsample_rate=downsample_rate)
+            try:
+                #train_dataloader, test_dataloader = get_dataloaders('SRFilterTraining', batch_size=batch_size, downsample_rate=downsample_rate)
+                train_dataloader, test_dataloader = get_dataloaders(dataset_name, batch_size=batch_size, downsample_rate=downsample_rate)
+                
+                optimizers = []
+                for method_idx in range(len(methods)):
+                    optimizers.append(optimizer(methods[method_idx].net.parameters(), lr = lr))
+                    #methods[method_idx].clear_history()
+                    methods[method_idx].train_mode()
+                    if downsample_rate_idx > 0:
+                        methods[method_idx].clear_history()
+
+                early_stopped = [False for _ in range(len(methods))]
+                #early_stopping = EarlyStopping(tolerance=tolerance/10, min_percent=min_percent)
+
+                for epoch in range(0,epochs+1):
+                    if early_stopped == [True for _ in range(len(methods))]:
+                        break
+                        
+                    training(train_dataloader,device,methods,optimizers,optimized_losses,estimate_clear_region,early_stopped)
+
+                    if epoch%10==0:
+                        with torch.no_grad():
+                            for method_idx in range(len(methods)):
+                                if early_stopped[method_idx] == True:
+                                    print(f"{methods[method_idx].name} early stopped")
+                                    continue
+                                else:
+                                    methods[method_idx].eval_mode()
+                                    losses, metrics = testing(test_dataloader, device, methods[method_idx], epoch, estimate_clear_region)
+                                
+                                    methods[method_idx].record.loss_history.append([])
+                                    methods[method_idx].record.metric_history.append([])
+                                    log_str = "Downsample %d Epoch [%d/%d] %s: " % (downsample_rate, epoch, epochs, methods[method_idx].name)
+                                    for optimized_losses_idx in range(len(optimized_losses)):
+                                        methods[method_idx].record.tb_writer.add_scalar("Loss/loss_sum", losses[optimized_losses_idx], epoch)
+                                        methods[method_idx].record.loss_history[-1].append(losses[optimized_losses_idx])
+                                        log_str += "Loss %d: %.6f " % (optimized_losses_idx, methods[method_idx].record.loss_history[-1][-1])
+                                    for refocused_img_metrics_idx in range(len(refocused_img_metrics)):
+                                        methods[method_idx].record.tb_writer.add_scalar("Metric/%s"%refocused_img_metrics_name[refocused_img_metrics_idx], metrics[refocused_img_metrics_idx], epoch)
+                                        methods[method_idx].record.metric_history[-1].append(metrics[refocused_img_metrics_idx])
+                                        log_str += "Metric %s: %.6f " % (refocused_img_metrics_name[refocused_img_metrics_idx], methods[method_idx].record.metric_history[-1][-1])
+
+                                    if np.sum(methods[method_idx].record.loss_history[-1]) < methods[method_idx].record.best_loss:
+                                        print("Found better model: %.6f < %.6f" % (np.sum(methods[method_idx].record.loss_history[-1]), methods[method_idx].record.best_loss))
+                                        methods[method_idx].record.best_loss = np.sum(methods[method_idx].record.loss_history[-1])
+                                        methods[method_idx].save_model(os.path.join('model',methods[method_idx].name,'best_model'))
+
+                                    print(log_str)
+                                    '''
+                                    test_loss = losses[0]
+                                    early_stopping(test_loss)
+                                    print(f"Last_loss:{early_stopping.last_loss}, Counter:{early_stopping.counter}")
+                                    if early_stopping.early_stop:
+                                        print("Early stopped at epoch: ", epoch)
+                                        methods[method_idx].record.tb_writer.close()
+                                        early_stopped[method_idx] = True
+                                        break
+                                    '''
+
+
+        
+                downsample_rate_idx += 1
+
+                end = time.time()
+                time_in_min = (end-start)//60
+                training_time.append(int(time_in_min))
+
             
-            optimizers = []
-            for method_idx in range(len(methods)):
-                optimizers.append(optimizer(methods[method_idx].net.parameters(), lr = lr))
-                #methods[method_idx].clear_history()
-                methods[method_idx].train_mode()
-                if downsample_rate_idx > 0:
-                    methods[method_idx].clear_history()
-
-            early_stopped = [False for _ in range(len(methods))]
-            #early_stopping = EarlyStopping(tolerance=tolerance/10, min_percent=min_percent)
-
-            for epoch in range(0,epochs+1):
-                if early_stopped == [True for _ in range(len(methods))]:
-                    break
-                    
-                training(train_dataloader,device,methods,optimizers,optimized_losses,estimate_clear_region,early_stopped)
-
-                if epoch%10==0:
-                    with torch.no_grad():
-                        for method_idx in range(len(methods)):
-                            if early_stopped[method_idx] == True:
-                                print(f"{methods[method_idx].name} early stopped")
-                                continue
-                            else:
-                                methods[method_idx].eval_mode()
-                                losses, metrics = testing(test_dataloader, device, methods[method_idx], epoch, estimate_clear_region)
-                            
-                                methods[method_idx].record.loss_history.append([])
-                                methods[method_idx].record.metric_history.append([])
-                                log_str = "Downsample %d Epoch [%d/%d] %s: " % (downsample_rate, epoch, epochs, methods[method_idx].name)
-                                for optimized_losses_idx in range(len(optimized_losses)):
-                                    methods[method_idx].record.tb_writer.add_scalar("Loss/loss_sum", losses[optimized_losses_idx], epoch)
-                                    methods[method_idx].record.loss_history[-1].append(losses[optimized_losses_idx])
-                                    log_str += "Loss %d: %.6f " % (optimized_losses_idx, methods[method_idx].record.loss_history[-1][-1])
-                                for refocused_img_metrics_idx in range(len(refocused_img_metrics)):
-                                    methods[method_idx].record.tb_writer.add_scalar("Metric/%s"%refocused_img_metrics_name[refocused_img_metrics_idx], metrics[refocused_img_metrics_idx], epoch)
-                                    methods[method_idx].record.metric_history[-1].append(metrics[refocused_img_metrics_idx])
-                                    log_str += "Metric %s: %.6f " % (refocused_img_metrics_name[refocused_img_metrics_idx], methods[method_idx].record.metric_history[-1][-1])
-
-                                if np.sum(methods[method_idx].record.loss_history[-1]) < methods[method_idx].record.best_loss:
-                                    print("Found better model: %.6f < %.6f" % (np.sum(methods[method_idx].record.loss_history[-1]), methods[method_idx].record.best_loss))
-                                    methods[method_idx].record.best_loss = np.sum(methods[method_idx].record.loss_history[-1])
-                                    methods[method_idx].save_model(os.path.join('model',methods[method_idx].name,'best_model'))
-
-                                print(log_str)
-                                '''
-                                test_loss = losses[0]
-                                early_stopping(test_loss)
-                                print(f"Last_loss:{early_stopping.last_loss}, Counter:{early_stopping.counter}")
-                                if early_stopping.early_stop:
-                                    print("Early stopped at epoch: ", epoch)
-                                    methods[method_idx].record.tb_writer.close()
-                                    early_stopped[method_idx] = True
-                                    break
-                                '''
-
-
-    
-            downsample_rate_idx += 1
-
-            end = time.time()
-            time_in_min = (end-start)//60
-            training_time.append(int(time_in_min))
-
-            '''
             except RuntimeError:
                 batch_size //= 2
                 print("Batchsize too large. Use batchsize = %d"% batch_size)
@@ -152,7 +153,7 @@ if __name__=="__main__":
                 if batch_size<1:
                     print("Batchsize is zero!")
                     break
-            '''
+            
             
 
         print(training_time)
