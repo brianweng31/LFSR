9ee6efc558cad6d718da9558070d7a21fd7fa332
brianweng31 <brian.weng31@gmail.com>
2022-08-04 19:21:37 +0800

    8_4_19:21

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ lr = 0.0001
 
 ### model selection
 model = "FilterBankMethod" #FilterBankMethod, LinearFilter, BaselineMethod
-model_idx = "F0"
+model_idx = "test"
 post_fix = ""
 pre_fix = ""
 
@@ -26,11 +26,11 @@ assert len(refocused_img_metrics) == len(refocused_img_metrics_name)
 
 ## training
 dataset_name = "RandomTraining" # HCI, RandomTraining
-training_light_field_downsample_rate = [4,2,1]
+training_light_field_downsample_rate = [2,1]
 #training_light_field_downsample_rate = [1]
-training_light_field_epoch = [40000,20000,20000]
+training_light_field_epoch = [20000,20000]
 #training_light_field_epoch = [30000]
-batch_size = 16
+batch_size = 4
 assert len(training_light_field_downsample_rate)==len(training_light_field_epoch)
 
 ## early stopping
