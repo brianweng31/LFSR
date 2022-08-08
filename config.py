48e3a607557d978dcbb2d69c57f7406157a08e95
brianweng31 <brian.weng31@gmail.com>
2022-08-08 10:10:20 +0800

    8_8_10:10

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ optimizer = torch.optim.Adam
 lr = 0.0001
 
 ### model selection
-model = "LinearFilter" #FilterBankMethod, LinearFilter, BaselineMethod
-model_idx = "F0"
+model = "FilterBankMethod" #FilterBankMethod, LinearFilter, BaselineMethod
+model_idx = "F1_v2"
 post_fix = ""
 pre_fix = ""
 
 ## loss
-optimized_losses = [nn.L1Loss()]
-#optimized_losses = [nn.MSELoss()]
+#optimized_losses = [nn.L1Loss()]
+optimized_losses = [nn.MSELoss()]
 loss_weights = [1.0]
 estimate_clear_region = False
 assert len(optimized_losses) == len(loss_weights)
@@ -25,11 +25,11 @@ refocused_img_metrics_name = ["PSNR", "SSIM", "GMSD", "LPIPS"]
 assert len(refocused_img_metrics) == len(refocused_img_metrics_name)
 
 ## training
-dataset_name = "RandomTraining" # HCI, RandomTraining
-#training_light_field_downsample_rate = [4,2,1]
-training_light_field_downsample_rate = [1]
-#training_light_field_epoch = [40000,20000,20000]
-training_light_field_epoch = [30000]
+dataset_name = "HCI" # HCI, RandomTraining
+training_light_field_downsample_rate = [4,2,1]
+#training_light_field_downsample_rate = [1]
+training_light_field_epoch = [40000,20000,20000]
+#training_light_field_epoch = [30000]
 batch_size = 16
 assert len(training_light_field_downsample_rate)==len(training_light_field_epoch)
 
