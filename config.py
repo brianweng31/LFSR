dd819809fb5c7a1d54a4884a1af61a1f7d48f506
brianweng31 <brian.weng31@gmail.com>
2022-08-10 12:06:38 +0800

    8_10_12:06

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ optimizer = torch.optim.Adam
 lr = 0.0001
 
 ### model selection
-model = "BaselineMethod" #FilterBankMethod, LinearFilter, BaselineMethod
+model = "LinearFilter" #FilterBankMethod, LinearFilter, BaselineMethod
 model_idx = "F1"
 post_fix = ""
 pre_fix = ""
