b4b0f14d2abf973f370f8a0a0fcded90821d4fdb
brianweng31 <brian.weng31@gmail.com>
2022-08-13 22:15:53 +0800

    8_13_22:15

--- test.py
+++ test.py
@@ -28,12 +28,12 @@ def testing(dataloader, device, model, epoch=0, estimate_clear_region=False):
         print(sample_batched_reshaped[0,0,:,:7,:7])
         '''
         lr = model.downsampling(sample_batched_reshaped)
-        '''
+        
         # b, st, c, h, w = lf.size()
         print(lr.size())
         print('down_lf[0] top left first row')
         print(lr[0,0,:,0])
-
+        '''
         print('correct answer = ')
         correct_answer = sample_batched_reshaped[:, :, :, 3::3,3::3]
         print(correct_answer.size())
