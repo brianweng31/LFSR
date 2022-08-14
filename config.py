bf10e4e8813ea440374e34ac0e085a41791a168f
brianweng31 <brian.weng31@gmail.com>
2022-08-14 13:39:31 +0800

    8_14_13:39

--- config.py
+++ config.py
@@ -4,17 +4,18 @@ import piq
 
 ### hyperparameters
 optimizer = torch.optim.Adam
-lr = 0.0001
+#lr = 0.0001
+lr = 0.00001
 
 ### model selection
 model = "LinearFilter" #FilterBankMethod, LinearFilter, BaselineMethod
-model_idx = "F0"
+model_idx = "F1"
 post_fix = ""
 pre_fix = ""
 
 ## loss
-optimized_losses = [nn.L1Loss()]
-#optimized_losses = [nn.MSELoss()]
+#optimized_losses = [nn.L1Loss()]
+optimized_losses = [nn.MSELoss()]
 loss_weights = [1.0]
 estimate_clear_region = False
 assert len(optimized_losses) == len(loss_weights)
@@ -25,7 +26,7 @@ refocused_img_metrics_name = ["PSNR", "SSIM", "GMSD", "LPIPS"]
 assert len(refocused_img_metrics) == len(refocused_img_metrics_name)
 
 ## training
-dataset_name = "RandomTraining" # RandomTraining, HCI, INRIA_Lytro 
+dataset_name = "HCI" # RandomTraining, HCI, INRIA_Lytro 
 #training_light_field_downsample_rate = [4,2,1]
 training_light_field_downsample_rate = [1]
 #training_light_field_epoch = [40000,20000,20000]
