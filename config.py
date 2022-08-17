47bd0f5db190d40c24390ad1d723ade556fee7c7
brianweng31 <brian.weng31@gmail.com>
2022-08-17 10:32:38 +0800

    8_17_10:32

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ lr = 0.0001
 #lr = 0.00001
 
 ### model selection
-model = "FilterBankMethod" #FilterBankMethod, LinearFilter, BaselineMethod
+model = "LinearFilter" #FilterBankMethod, LinearFilter, BaselineMethod
 model_idx = "F1_G1"
 post_fix = ""
 pre_fix = ""
@@ -27,15 +27,15 @@ assert len(refocused_img_metrics) == len(refocused_img_metrics_name)
 
 ## training
 dataset_name = "HCI" # RandomTraining, HCI, INRIA_Lytro 
-training_light_field_downsample_rate = [4,2,1]
-#training_light_field_downsample_rate = [1]
-training_light_field_epoch = [40000,20000,20000]
-#training_light_field_epoch = [30000]
+#training_light_field_downsample_rate = [4,2,1]
+training_light_field_downsample_rate = [1]
+#training_light_field_epoch = [40000,20000,20000]
+training_light_field_epoch = [30000]
 batch_size = 4
 assert len(training_light_field_downsample_rate)==len(training_light_field_epoch)
 
 ## early stopping
-tolerance = 1000
+tolerance = 2000
 min_percent = 0.001
 
 #tolerance = 20
