e9f9b0caa38adda925d518436bb970b2d5abf5cc
brianweng31 <brian.weng31@gmail.com>
2022-09-30 18:05:04 +0800

    9_30_18:05

--- model.py
+++ model.py
@@ -363,7 +363,7 @@ class FilterBankKernel(nn.Module):
         outputs = []
         for i in range(self.s):
             for j in range(self.t):
-                x1 = x_[:,[i*self.t+j],:,:,:]
+                x1 = x_[:,i*self.t+j,:,:,:]
                 x1 = torch.roll(x1, shifts=(-(i-1), -(j-1)), dims=(-2,-1))
                 x1_out = F.conv2d(x1, filter_, stride=3, padding='same')
                 outputs.append(x1_out)
