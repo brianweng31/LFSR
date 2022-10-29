962fb823ea58e21668dd892c451e28a97b2425d8
brianweng31 <brian.weng31@gmail.com>
2022-10-29 13:29:25 +0800

    10_29_13:29

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ training_light_field_downsample_rate = [4,2,1]
 #training_light_field_downsample_rate = [1]
 #training_light_field_epoch = [40000,20000,20000]
 training_light_field_epoch = [10,10,10]
-training_light_field_epoch = [1]
+#training_light_field_epoch = [1]
 batch_size = 16
 assert len(training_light_field_downsample_rate)==len(training_light_field_epoch)
 
