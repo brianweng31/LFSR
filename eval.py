d87e60a23a3248547c53f1f3a3fc0f6404d03294
brianweng31 <brian.weng31@gmail.com>
2022-12-10 23:55:29 +0800

    12_10_23:55

--- eval.py
+++ eval.py
@@ -138,7 +138,7 @@ down_lf = np.moveaxis(down_lf, 2, -1)
 if not os.path.isdir('npy'):
     os.mkdir('npy')
 np.save(f'npy/down_{model_idx}',down_lf)
-np.save(f'npy/gau_baseline_-2',light_field)
+np.save(f'npy/gau_baseline',light_field)
 '''
 try:
     #estimate_clear_regions = np.array(estimate_clear_regions.detach().cpu())
